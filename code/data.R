53df776d08c4504fd5c0bca7466618c2e45b4d7a
Jason Miller <Jason.Miller@laureate.net>
2017-05-03 16:13:33 -0400

    .

--- code/data.R
+++ code/data.R
@@ -24,9 +24,12 @@ if(reload){
   dat       <- scrape("2015-01-01", "2017-12-31") # Features: "atbat"  "action" "pitch"  "po" "runner"
   locations <- select(dat$pitch, pitch_type, start_speed, px, pz, des, num, gameday_link)
   names     <- select(dat$atbat, pitcher, batter, pitcher_name, batter_name, num, gameday_link, event, stand)
-  data      <- names %>% 
-    filter(pitcher_name == "Jacob DeGrom") %>% 
-    inner_join(locations, ., by = c("num", "gameday_link"))
+  # data      <- names %>% 
+  #   filter(pitcher_name == "Jacob DeGrom") %>% 
+  #   inner_join(locations, ., by = c("num", "gameday_link"))
+  
+  #db <- src_sqlite("pitchfx.sqlite3", create = T)
+  #scrape(start = "2008-01-01", end = Sys.Date(), connect = db$con)
   
 }
 
--- code/viz.R
+++ code/viz.R
@@ -1,1 +1,8 @@
-viz
+# adding ggplot2 functions to customize animateFX() output won't work, but
+# you can pass a list to the layer argument like this:
+x <- list(
+  facet_grid(pitcher_name ~ stand, labeller = label_both), 
+  theme_bw(), 
+  coord_equal()
+)
+animateFX(pitches, layer = x)
